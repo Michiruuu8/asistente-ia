afffa40fcf1a34f81552b9d70d029430750a307e
Michiruuu8 <michiru.cortes78@gmail.com>
2026-08-07 17:04:15 -0600

    feat: conectar clasificador de intencion al endpoint /chat

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from model.predict import predict
 
 app = FastAPI(title="Asistente IA - Backend")
+
+# Definimos la forma que debe tener el mensaje que nos manden
+class Mensaje(BaseModel):
+    texto: str
 
 @app.get("/")
 def home():
     return {"mensaje": "El backend del asistente esta funcionando"}
+
+@app.post("/chat")
+def chat(mensaje: Mensaje):
+    intencion, confianza = predict(mensaje.texto)
+    return {
+        "texto_recibido": mensaje.texto,
+        "intencion": intencion,
+        "confianza": round(confianza, 1)
+    }
